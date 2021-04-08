78b919f49b80f6fd6675a04613e4c66e5988927a
Maciej Gryka <maciej@rainforestapp.com>
2021-04-08 17:36:45 +0200

    make checkbox saner

--- lib/regex_help_web/live/page_live.ex
+++ lib/regex_help_web/live/page_live.ex
@@ -16,20 +16,15 @@ defmodule RegexHelpWeb.PageLive do
     {:noreply, build_query(socket, query, socket.assigns.flags)}
   end
 
-  def handle_event("set_flag_repetitions", params, socket) do
-    IO.inspect(params)
-    enabled = Map.get(params, "value") == "true"
-    flags = %RegexHelper.Flags{socket.assigns.flags | repetitions: enabled}
-
-
+  def handle_event("set_flag_repetitions", %{"enabled" => enabled}, socket) do
+    flags = %RegexHelper.Flags{socket.assigns.flags | repetitions: enabled == "true"}
     {:noreply, build_query(socket, socket.assigns.query, flags)}
   end
 
   defp build_query(socket, query, flags) do
-    new_expression = RegexHelper.build(query, flags)
     socket
     |> assign(query: query)
     |> assign(flags: flags)
-    |> assign(help_response: new_expression)
+    |> assign(help_response: RegexHelper.build(query, flags))
   end
 end
