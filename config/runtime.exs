a4920c52386b69d8b1790c039aea3b21ff0c008e
Maciej Gryka <maciej@rainforestapp.com>
2021-04-29 20:59:28 +0200

    add regex.help domain to the config

--- config/runtime.exs
+++ config/runtime.exs
@@ -13,6 +13,10 @@ if config_env() == :prod do
   config :regex_help, RegexHelpWeb.Endpoint,
     server: true,
     url: [host: "#{app_name}.fly.dev", port: 80],
+    check_origin: [
+      "https://regex.help",
+      "https://#{app_name}.fly.dev",
+    ],
     http: [
       port: String.to_integer(System.get_env("PORT") || "4000"),
       transport_options: [socket_opts: [:inet6]]
