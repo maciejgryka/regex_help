e9e44218844d390a963623c5d302deea6a86625e
Maciej Gryka <maciej@rainforestapp.com>
2021-04-30 16:37:00 +0200

    fix formatting

--- config/prod.exs
+++ config/prod.exs
@@ -1,4 +1,3 @@
 import Config
 
-config :regex_help, RegexHelpWeb.Endpoint,
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+config :regex_help, RegexHelpWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
