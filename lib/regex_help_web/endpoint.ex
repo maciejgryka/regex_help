c309b8429d19c7331965e130407a8ed1154470cd
Maciej Gryka <maciej@rainforestapp.com>
2021-05-06 20:54:07 +0200

    remove [[statics]]

--- lib/regex_help_web/endpoint.ex
+++ lib/regex_help_web/endpoint.ex
@@ -23,7 +23,7 @@ defmodule RegexHelpWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :regex_help,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
