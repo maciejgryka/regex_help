d60662a5450b817d66439194a572bc3436539cbc
Maciej Gryka <maciej@rainforestapp.com>
2021-05-07 10:50:44 +0200

    add flag descriptions and make them prettier

--- lib/regex_help/regex_helper.ex
+++ lib/regex_help/regex_helper.ex
@@ -2,12 +2,12 @@ defmodule RegexHelper do
   use Rustler, otp_app: :regex_help, crate: "regexhelper"
 
   defmodule Flags do
-    defstruct digits: %{value: false, description: "Convert any Unicode decimal digit to \d"},
-              spaces: %{value: false, description: "Convert any Unicode whitespace character to \s"},
-              words: %{value: false, description: "Convert any Unicode word character to \w"},
-              repetitions: %{value: false, description: "Detect repeated non-overlapping substrings and converts them to {min,max} quantifier notation"},
-              ignore_case: %{value: false, description: "Perform case-insensitive matching, letters match both upper and lower case"},
-              capture_groups: %{value: false, description: "Replace non-capturing groups by capturing ones"}
+    defstruct digits: %{value: false, description: "Convert digits to \\d"},
+              spaces: %{value: false, description: "Convert whitespace to \\s"},
+              words: %{value: false, description: "Convert any Unicode word character to \\w"},
+              repetitions: %{value: false, description: "Convert repeated substrings to {min,max}"},
+              ignore_case: %{value: false, description: "Ignore capitalization"},
+              capture_groups: %{value: false, description: "Use capturing groups"}
   end
 
   def build(s, flags) do
