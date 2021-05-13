9fd31b8216775b6e8df3c03d14e77c1fe2789518
Maciej Gryka <maciej@rainforestapp.com>
2021-05-13 14:10:16 +0200

    enable live dashboard in prod

--- lib/regex_help_web/router.ex
+++ lib/regex_help_web/router.ex
@@ -1,5 +1,6 @@
 defmodule RegexHelpWeb.Router do
   use RegexHelpWeb, :router
+  import Phoenix.LiveDashboard.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -10,8 +11,8 @@ defmodule RegexHelpWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
+  pipeline :admins_only do
+    plug :admin_basic_auth
   end
 
   scope "/", RegexHelpWeb do
@@ -21,24 +22,14 @@ defmodule RegexHelpWeb.Router do
     live "/", PageLive, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", RegexHelpWeb do
-  #   pipe_through :api
-  # end
+  scope "/" do
+    pipe_through [:browser, :admins_only]
+    live_dashboard "/dashboard", metrics: RegexHelpWeb.Telemetry
+  end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: RegexHelpWeb.Telemetry
-    end
+  defp admin_basic_auth(conn, _opts) do
+    username = System.fetch_env!("AUTH_USERNAME")
+    password = System.fetch_env!("AUTH_PASSWORD")
+    Plug.BasicAuth.basic_auth(conn, username: username, password: password)
   end
 end
