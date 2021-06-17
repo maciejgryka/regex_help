e39a475cd238d78bbfd922fac37a5abc1f8e99c7
Maciej Gryka <maciej@rainforestapp.com>
2021-06-17 12:26:07 +0200

    update rustler config

--- mix.exs
+++ mix.exs
@@ -7,8 +7,7 @@ defmodule RegexHelp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext, :rustler] ++ Mix.compilers(),
-      rustler_crates: [regexhelper: []],
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
