e3b524eeb2020573834bff04b86559a66e538000
Maciej Gryka <maciej@rainforestapp.com>
2021-11-15 10:36:15 +0100

    fix a test

--- test/regex_help_web/live/page_live_test.exs
+++ test/regex_help_web/live/page_live_test.exs
@@ -4,8 +4,15 @@ defmodule RegexHelpWeb.PageLiveTest do
   import Phoenix.LiveViewTest
 
   test "disconnected and connected render", %{conn: conn} do
+    conn = Map.put(conn, :host, "localhost")
     {:ok, page_live, disconnected_html} = live(conn, "/")
     assert disconnected_html =~ "Enter your examples here:"
     assert render(page_live) =~ "Enter your examples here:"
+  end
+
+  test "non-localhost requests get redirected to HTTPS", %{conn: conn} do
+    original_host = conn.host
+    redirected_to = "https://#{original_host}/"
+    {:error, {:redirect, %{to: ^redirected_to}}} = live(conn, "/")
   end
 end
